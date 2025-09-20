223e773b25370a10db92bfaf910ebeb875fecebb
Ralph Maron A. Eda <128874876+ralphmarondev@users.noreply.github.com>
2025-09-20 11:57:00 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+	<main class="h-screen w-screen bg-pink-50 flex items-center justify-center">
+		<h4 class="text-2xl text-pink-800">Welcome to Software Development Club</h4>
+	</main>
+</template>
